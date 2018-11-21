a7c0547a24da02f55a9810b9b4fef19466feaf37
柯贤铭 <kexianming@testin.cn>
2018-11-21 16:28:09 +0800

    测试提交到mytest分支

---
+++ src/main/java/com/zh/test/TestPojo.java
@@ -0,0 +1,26 @@
+package com.zh.test;
+
+/**
+ * ******************************
+ * Author：      柯贤铭
+ * CreateTime:   2018/11/21 16:27
+ * Description:  TODO
+ * Version:      V1.0
+ * ******************************
+ */
+public class TestPojo {
+
+    /**
+     * a : 123
+     */
+
+    private String a;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
